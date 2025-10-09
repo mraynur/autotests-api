b36e40e298ced0b236518a71e159e65f78051602
mraynur <m.raynur@gmail.com>
2025-10-09 14:43:02 +0300

    Практикуемся в использовании API клиентов

--- clients/users/public_users_client.py
+++ clients/users/public_users_client.py
@@ -1,6 +1,17 @@
 from typing import TypedDict
 from httpx import Response
 from clients.api_client import APIClient
+from clients.public_http_builder import get_public_http_client
+
+class User(TypedDict):
+    """
+    Описание структуры пользователя.
+    """
+    id: str
+    email: str
+    lastName: str
+    firstName: str
+    middleName: str
 
 class CreateUserRequestDict(TypedDict):
     """
@@ -12,9 +23,15 @@ class CreateUserRequestDict(TypedDict):
     firstName: str
     middleName: str
 
+class CreateUserResponseDict(TypedDict):
+    """
+    Описание структуры ответа создания пользователя.
+    """
+    user: User
+
 class PublicUsersClient(APIClient):
     """
-    Клиент для работы с /api/v1/users
+    Клиент для работы с /api/v1/users (без авторизации)
     """
     def create_user_api(self, request: CreateUserRequestDict) -> Response:
         """
@@ -23,3 +40,14 @@ class PublicUsersClient(APIClient):
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.post("/api/v1/users", json=request)
+
+    def create_user(self, request: CreateUserRequestDict) -> CreateUserResponseDict:
+        response = self.create_user_api(request)
+        return response.json()
+
+def get_public_users_client() -> PublicUsersClient:
+    """
+    Функция создаёт экземпляр PublicUsersClient с уже настроенным HTTP-клиентом.
+    :return: Готовый к использованию PublicUsersClient.
+    """
+    return PublicUsersClient(client=get_public_http_client())
