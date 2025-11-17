d6a7e63062efc111ba7328959fcb82a997f46776
mraynur <m.raynur@gmail.com>
2025-11-17 18:58:26 +0300

    Практикуемся в написании API автотестов. Часть 2

--- clients/courses/courses_schema.py
+++ clients/courses/courses_schema.py
@@ -54,7 +54,19 @@ class UpdateCourseRequestSchema(BaseModel):
     model_config = ConfigDict(populate_by_name=True)
 
     title: str | None = Field(default_factory=fake.sentence)
-    maxScore: int | None = Field(alias="maxScore", default_factory=fake.max_score)
-    minScore: int | None = Field(alias="minScore", default_factory=fake.min_score)
+    max_score: int | None = Field(alias="maxScore", default_factory=fake.max_score)
+    min_score: int | None = Field(alias="minScore", default_factory=fake.min_score)
     description: str | None = Field(default_factory=fake.text)
-    estimatedTime: str | None = Field(alias="estimatedTime", default_factory=fake.estimated_time)
+    estimated_time: str | None = Field(alias="estimatedTime", default_factory=fake.estimated_time)
+
+class UpdateCourseResponseSchema(BaseModel):
+    """
+    Описание структуры ответа обновления курса.
+    """
+    course: CourseSchema
+
+class GetCoursesResponseSchema(BaseModel):
+    """
+    Описание структуры ответа на получение списка курсов.
+    """
+    courses: list[CourseSchema]
