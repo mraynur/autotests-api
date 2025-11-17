b7e1713d1b40e3595bf2cf6376f997d35379fb6e
mraynur <m.raynur@gmail.com>
2025-11-17 12:09:48 +0300

    Практикуемся в написании API автотестов. Часть 1

--- clients/files/files_schema.py
+++ clients/files/files_schema.py
@@ -23,3 +23,9 @@ class CreateFileResponseSchema(BaseModel):
     Описание структуры ответа создания файла.
     """
     file: FileSchema
+
+class GetFileResponseSchema(BaseModel):
+    """
+    Описание структуры запроса получения файла.
+    """
+    file: FileSchema
